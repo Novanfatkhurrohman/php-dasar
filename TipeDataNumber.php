4065cca6f992fddce6f15ee52c391452ee2b8408
Novanfatkhurrohman <novanfatkhurrohman9@gmail.com>
2021-06-21 15:09:16 +0700

    first commit

---
+++ TipeDataNumber.php
@@ -0,0 +1,36 @@
+<?php
+
+//bilangan bulat mengggunakan interger
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b111);
+
+echo "Underscore di number : ";
+var_dump(1_40_567);
+
+//bialngan pecahan menggunakan foating point    
+
+echo "Floating point : ";
+var_dump(1.345);
+
+echo "foatng point dengan E notation plus : ";
+var_dump(1.2e3); //(1.2x1000)
+
+echo "foatng point dengan E notation minus : ";
+var_dump(1e-3); //(1x0.001)
+
+echo "underscore di floating point : ";
+var_dump(1_234.567);
+
+//interger overflow
+echo "interger overflow : ";
+var_dump(8849698698691374747441);
---
+++ TipeDataString.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'nama : ';
+echo 'Novan fakhurrohman';
+echo "\n";
+echo "belajar java\t belajar php";
